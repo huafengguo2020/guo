796ca7ade39ce8391af5ec45bdbcdf8f60dc8695
huafengguo2020 <994302768@qq.com>
2022-12-03 19:54:18 +0800

    我的第一个java程序

---
+++ java-foundational/src/main/java/com/guo/foundational/object/HelloWord.java
@@ -0,0 +1,14 @@
+package com.guo.foundational.object;
+
+/**
+ * helloword
+ *
+ * @author huafengguo2020
+ * @version 2022/12/3 19:51
+ * @since JDK8
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
